f1714eabeedffdb6fa2748961cd0a41cbbe03c1d
Piyavva <piyavkin05@bk.ru>
2024-11-03 22:36:09 +0300

    Fix

--- backend/cmd/service/main.go
+++ backend/cmd/service/main.go
@@ -8,16 +8,8 @@ import (
 
 func main() {
 	cfg := config.Load("config/config.yaml")
-	db, err := database.NewDB(cfg)
+	_, err := database.NewDB(cfg)
 	if err != nil {
 		log.Fatal(err)
-	}
-	defer db.Db.Close()
-	_, err = db.Db.Exec(`CREATE TABLE IF NOT EXISTS books (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL
-    );`)
-	if err != nil {
-		log.Fatal("Ошибка создания таблицы:", err)
 	}
 }
