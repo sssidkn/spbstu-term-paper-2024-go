482dfde3f58caf9853fa44893e9fa86db788f8eb
Piyavva <piyavkin05@bk.ru>
2024-11-03 21:11:13 +0300

    Add creating db

--- backend/cmd/service/main.go
+++ backend/cmd/service/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"github.com/backend/database"
 	"github.com/backend/internal/config"
+	"log"
 )
 
 func main() {
 	cfg := config.Load("config/config.yaml")
-	fmt.Println(*cfg)
+	db, err := database.NewDB(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Db.Close()
+	_, err = db.Db.Exec(`CREATE TABLE IF NOT EXISTS books (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(255) NOT NULL
+    );`)
+	if err != nil {
+		log.Fatal("Ошибка создания таблицы:", err)
+	}
 }
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/backend
 
 go 1.23.1
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	github.com/lib/pq v1.10.9
+	gopkg.in/yaml.v3 v3.0.1
+)
